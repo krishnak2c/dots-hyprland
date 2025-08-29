fde19c81004eb46e2a1b1f616b1474bb95a65196
Krishna <krishchoudhary2126@gmail.com>
2025-08-30 03:02:08 +0530

    modified:config files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,65 @@
-function fish_prompt -d "Write out the prompt"
-    # This shows up as USER@HOST /home/user/ >, with the directory colored
-    # $USER and $hostname are set by fish, so you can just use them
-    # instead of using `whoami` and `hostname`
-    printf '%s@%s %s%s%s > ' $USER $hostname \
-        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+set -g fish_greeting
+
+
+if type -q starship
+    starship init fish | source
+    set -gx STARSHIP_CACHE $XDG_CACHE_HOME/starship
+    set -gx STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/starship.toml
 end
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set fish_greeting
 
+if type -q duf
+    function df -d "Run duf with last argument if valid, else run duf"
+        if set -q argv[-1] && test -e $argv[-1]
+            duf $argv[-1]
+        else
+            duf
+        end
+    end
 end
 
-starship init fish | source
-if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-    cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-end
+# fzf
 
-alias pamcan pacman
-alias ls 'eza --icons'
-alias clear "printf '\033[2J\033[3J\033[1;1H'"
-alias q 'qs -c ii'
+
+
+# NOTE: binds Alt+n to inserting the nth command from history in edit buffer
+# e.g. Alt+4 is same as pressing Up arrow key 4 times
+# really helpful if you get used to it
+bind_M_n_history
+
+
+
+# example integration with bat : <cltr+f>
+# bind -M insert \ce '$EDITOR $(fzf --preview="bat --color=always --plain {}")'
+
+
+set fish_pager_color_prefix cyan
+set fish_color_autosuggestion brblack
+
+# List Directory
+alias c='clear'
+alias l='eza -lh --icons=auto'
+alias ls='eza -1 --icons=auto'
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first'
+alias ld='eza -lhD --icons=auto'
+alias lt='eza --icons=auto --tree'
+alias un='sudo $aurhelper -Rns $argv'
+alias up='sudo $aurhelper -Syu $argv'
+alias pl='$aurhelper -Qs'
+alias pa='sudo $aurhelper -S $argv'
+alias pc='$aurhelper -Sc'
+alias po='$aurhelper -Qtdq | $aurhelper -Rns -'
+alias vc='codium'
+alias fastfetch='fastfetch --config ~/.config/fastfetch/config.jsonc \
+          --logo-type kitty --logo "$(ls "$LOGO_DIR"/*.icon | shuf -n 1)"'
+
+# Directory navigation shortcuts
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# Add Flutter to PATH
+set -gx PATH $PATH /home/krishna/flutter/bin
     
-
-# function fish_prompt
-#   set_color cyan; echo (pwd)
-#   set_color green; echo '> '
-# end
---
+++ .config/fish/functions/fish_greeting.fish
@@ -0,0 +1,4 @@
+function fish_greeting
+    set_color normal
+    fastfetch-random
+end
